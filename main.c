52fdf17a3c1f2670f43a5ad40cb3d346566731ed
Areii1 <ari.jaaskelainen1@gmail.com>
2016-09-26 22:31:59 +0300

    add build_matrix function

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// int[][] get_a_matrix(int rows, int columns);
+int build_matrix(int rows, int columns);
+// void print_matrix(int *, int rows, int columns);
 
 int main(void)
 {
@@ -15,5 +16,63 @@ int main(void)
 
 	printf("preferred matrix: %dx%d\n", rows, columns);
 
+	build_matrix(rows, columns);
+
 	return 0;
 }
+
+int build_matrix(int rows, int columns)
+{
+	int matrix[rows][columns];
+
+	int x, y;
+
+	for (y = 0; y < rows; y++)
+	{
+		for (x = 0; x < columns; x++)
+			{
+			printf("give y = %d, x = %d, entry\n", y, x);
+			scanf("%d", &matrix[y][x]);
+			printf("got value = %d\n", matrix[y][x]);
+			}
+	}
+
+	printf("-----------   AFTER VALUE  ASSINGMENTS   ----------\n");
+
+	for (y = 0; y < rows; y++)
+	{
+		for (x = 0; x < columns; x++)
+		{
+			printf("matrix[%d][%d] = %d\n", y, x, matrix[y][x]);
+		}
+	}
+
+
+	return 0;
+}
+
+
+
+/*
+void print_matrix(int *matrix, int rows, int columns)
+{
+	int y, x;
+	for (y = 0; y < rows; y++)
+	{
+		for (x = 0; x < columns; x++)
+		{
+			printf("matrix[y][x] = %d\n", matrix[y][x]);
+		}
+	}
+}
+*/
+
+
+
+
+
+
+
+
+
+
