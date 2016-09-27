7f657f3860415aeb10adeb231e39fdc72a282108
Areii1 <ari.jaaskelainen1@gmail.com>
2016-09-27 22:52:11 +0300

    allocate memory outside of build_matrix for the matrix

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int build_matrix(int rows, int columns);
-// void print_matrix(int *, int rows, int columns);
+int build_matrix(int* matrix[], int rows, int columns);
+// void print_matrix(int** matrix, int rows, int columns);
+// int copy_matrix(int *destination_matrix, int *source_matrix);
 
 int main(void)
 {
-	int rows, columns;
+	int rows, columns, i;
+	int **first_matrix;
 
 	printf("give number of rows\n");
 	scanf("%d", &rows);
@@ -15,15 +17,29 @@ int main(void)
 	scanf("%d", &columns);
 
 	printf("preferred matrix: %dx%d\n", rows, columns);
+	
+	first_matrix = malloc(rows * sizeof *first_matrix);
+	for (i = 0; i < rows; i++)
+	{
+		first_matrix[i] = malloc(columns * sizeof *first_matrix[i]);
+	}
 
-	build_matrix(rows, columns);
+	build_matrix(first_matrix, rows, columns);
+
+	for (i = 0; i < rows; i++)
+	{
+		free(first_matrix[i]);
+	}
+
+	free(first_matrix);
+	// print_matrix(*matrix, rows, columns);
 
 	return 0;
 }
 
-int build_matrix(int rows, int columns)
+
+int build_matrix(int* matrix[], int rows, int columns)
 {
-	int matrix[rows][columns];
 
 	int x, y;
 
@@ -31,9 +47,9 @@ int build_matrix(int rows, int columns)
 	{
 		for (x = 0; x < columns; x++)
 			{
-			printf("give y = %d, x = %d, entry\n", y, x);
+			printf("give [%d,%d] element:\n", y, x);
 			scanf("%d", &matrix[y][x]);
-			printf("got value = %d\n", matrix[y][x]);
+			printf("got value %d\n", matrix[y][x]);
 			}
 	}
 
@@ -41,32 +57,38 @@ int build_matrix(int rows, int columns)
 
 	for (y = 0; y < rows; y++)
 	{
+		printf("\n");
+
 		for (x = 0; x < columns; x++)
 		{
-			printf("matrix[%d][%d] = %d\n", y, x, matrix[y][x]);
+			printf("%d\t", matrix[y][x]);
 		}
+
+		printf("\n");
 	}
 
 
 	return 0;
 }
 
-
-
 /*
 void print_matrix(int *matrix, int rows, int columns)
 {
-	int y, x;
+	int y, x;	
+	
 	for (y = 0; y < rows; y++)
 	{
+		printf("\n");
+
 		for (x = 0; x < columns; x++)
 		{
-			printf("matrix[y][x] = %d\n", matrix[y][x]);
+			printf("%d\t", matrix[y][x]);
 		}
+
+		printf("\n");
 	}
 }
 */
-
 
 
 
