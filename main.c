57d526c589dde70267aa189c9e9c59482bb3efc8
Areii1 <ari.jaaskelainen1@gmail.com>
2016-09-29 18:56:43 +0300

    refactor matrix measure requests and some variable names

--- main.c
+++ main.c
@@ -7,57 +7,55 @@
 
 int build_matrix(int* matrix[], int rows, int columns);
 void print_matrix(int* matrix[], int rows, int columns);
+void ask_for_measures(int a_or_b);
 
-int first_rows, first_columns, second_rows, second_columns;
+int A_rows, A_columns, B_rows, B_columns;
 
 int main(void)
 {
 	// ------------------------- FIRST MATRIX ---------------------------
 	int i;
-	int **first_matrix;
+	int **A;
 
-	// probe for measures
-	printf("matrix A #rows\n");
-	scanf("%d", &first_rows);
-	printf("matrix B #columns\n");
-	scanf("%d", &first_columns);
-	printf("A (%dx%d)\n\n\n", first_rows, first_columns);
+	// probe for measures on matrix A
+	ask_for_measures(1);
+	printf("A (%dx%d)\n\n\n", A_rows, A_columns);
 	
 	//allocate first matrix (2-d int array)
-	first_matrix = malloc(MAX_ROWS * sizeof *first_matrix);
+	A = malloc(MAX_ROWS * sizeof *A);
 	for (i = 0; i < MAX_ROWS; i++)
 	{
-		first_matrix[i] = malloc(MAX_COLUMNS * sizeof *first_matrix[i]);
+		A[i] = malloc(MAX_COLUMNS * sizeof *A[i]);
 	}
 
 	//pass allocated matrix for element probing
-	build_matrix(first_matrix, first_rows, first_columns);
+	build_matrix(A, A_rows, A_columns);
 	printf("-------------------- FIRST MATRIX -------------------\n");
-	print_matrix(first_matrix, first_rows, first_columns);
+	print_matrix(A, A_rows, A_columns);
 	printf("-----------------------------------------------------\n");
 
 //----------------------- SECOND MATRIX -----------------------
 	int j;
-	int **second_matrix;
+	int **B;
 
 	//probe for measures
 	printf("matrix B #rows\n");
-	scanf("%d", &second_rows);
+	scanf("%d", &B_rows);
 	printf("matrix B  #columns\n");
-	scanf("%d", &second_columns);
-	printf("B (%dx%d)\n\n\n", second_rows, second_columns);
+	scanf("%d", &B_columns);
+	printf("B (%dx%d)\n\n\n", B_rows, B_columns);
 	
 	//allocate second matrix (2-d int array)
-	second_matrix = malloc(MAX_ROWS * sizeof *second_matrix);
+	B = malloc(MAX_ROWS * sizeof *B);
 	for (j = 0; j < MAX_ROWS; j++)
 	{
-		second_matrix[j] = malloc(MAX_COLUMNS * sizeof *second_matrix[i]);
+		B[j] = malloc(MAX_COLUMNS * sizeof *B[i]);
 	}
 
 	//pass allocated matrix for element probing
-	build_matrix(second_matrix, second_rows, second_columns);
+	build_matrix(B, B_rows, B_columns);
 	printf("-------------------- SECOND MATRIX -------------------\n");
-	print_matrix(second_matrix, second_rows, second_columns);
+	print_matrix(B, B_rows, B_columns);
 	printf("------------------------------------------------------\n");
 	
 	int user_value;
@@ -86,20 +84,20 @@ int main(void)
 		printf("-------------------------------------------------------------\n");
 	}
 	*/
-	/*elseif*/if (first_rows == second_rows && first_columns == second_columns)
+	/*elseif*/if (A_rows == B_rows && A_columns == B_columns)
 	{
 		if (user_value == 1)
 		{
-			add_matrices(first_matrix, second_matrix, destination_matrix);
+			add_matrices(A, B, destination_matrix);
 			printf("-------------------- DESTINATION MATRIX -------------------\n");
-			print_matrix(destination_matrix, first_rows, first_columns);
+			print_matrix(destination_matrix, A_rows, A_columns);
 			printf("-----------------------------------------------------------\n");
 		}
 		else if (user_value == 2)
 		{
-			substract_matrices(first_matrix, second_matrix, destination_matrix);
+			substract_matrices(A, B, destination_matrix);
 			printf("-------------------- DESTINATION MATRIX -------------------\n");
-			print_matrix(destination_matrix, first_rows, first_columns);
+			print_matrix(destination_matrix, A_rows, A_columns);
 			printf("-----------------------------------------------------------\n");
 		}
 		else
@@ -115,13 +113,13 @@ int main(void)
 		// free unused matrix space
 	for (i = 0; i < MAX_ROWS; i++)
 	{
-		free(first_matrix[i]);
-		free(second_matrix[i]);
+		free(A[i]);
+		free(B[i]);
 		free(destination_matrix[i]);
 	}
 
-	free(first_matrix);
-	free(second_matrix);
+	free(A);
+	free(B);
 	free(destination_matrix);
 	
 	return 0;
@@ -173,4 +171,23 @@ void print_matrix(int* matrix[], int rows, int columns)
 		printf("\n");
 	}
 }
+
+void ask_for_measures(int a_or_b)
+{
+	if (a_or_b == 1)
+	{
+		printf("matrix A #rows\n");
+		scanf("%d", &A_rows);
+		printf("matrix B #columns\n");
+		scanf("%d", &A_columns);
+	}
+	else if (a_or_b == 2) {
+		printf("matrix A #rows\n");
+		scanf("%d", &B_rows);
+		printf("matrix B #columns\n");
+		scanf("%d", &B_columns);
+	}
+}
+
+
 
